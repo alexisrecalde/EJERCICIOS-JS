338167688055c2c1d85b214665d5770fbf8d2ea4
alexisrecalde <alexis.recalde@hotmail.com>
2022-01-28 19:26:54 -0300

    Add files via upload

---
+++ CURSO JS/JS/constantes.js
@@ -0,0 +1,7 @@
+export const PI = Math.PI;
+export let usuario = "Alexis";
+let contraseña = "asd";
+
+export default function saludar() {
+  console.log("holaaaa");
+}
---
+++ CURSO JS/JS/modulos.js
@@ -0,0 +1,8 @@
+import { PI, usuario } from "./constantes.js";
+import { aritmeticaa } from "./aritmeticas.js";
+
+console.log("Archivo modulos.js");
+
+console.log(PI, usuario);
+console.log(aritmeticaa.sumar(3, 4));
+//saludar();
